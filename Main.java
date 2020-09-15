4d57c924e023540f7cd4f9ecb189e0292ebe027c
seoe5580 <seoe5580@wrdsb.ca>
2020-09-15 19:16:20 +0000

    Finished the ascending number order assignment

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Allows you to see numbers in ascending order
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +11,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+    
+    // Asks the user to input a number
+    System.out.println("Please enter an integer:");
+
+    // Gets the number from user
+    int num1 = input.nextInt();
+
+    // Asks the user to input another number
+    System.out.println("Please enter another integer:");
+
+    // Gets the other number from user
+    int num2 = input.nextInt();
+
+    if (num1 > num2){
+      System.out.println("Your numbers in ascending order are " + num2 + ", " + num1);
+    } else {
+      System.out.println("Your numbers in ascending order are " + num1 + ", " +  num2);
+    }
     
   }
 }
